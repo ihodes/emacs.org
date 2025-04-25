deae43717dd621fda7cafb8a7f64e1c4440e1484
Isaac Hodes <isaachodes@gmail.com>
2025-04-24 22:42:08 -0400

    Switch to vertico + orderless + consult + embark for better completion

--- init.el
+++ init.el
@@ -23,11 +23,20 @@
  '(ocamlformat-enable 'enable-outside-detected-project)
  '(org-agenda-files nil)
  '(package-selected-packages
-   '(yasnippet swiper ivy typescript-mode adaptive-wrap visual-fill-column mmm-mode web-mode visual-regexp use-package undo-tree tuareg smex smart-mode-line rainbow-delimiters projectile paredit merlin magit hydra haskell-mode git-timemachine fullframe flycheck flx exec-path-from-shell ensime counsel clojure-mode browse-kill-ring ag ace-window ace-jump-mode)))
+   '(ace-jump-mode ace-window adaptive-wrap ag browse-kill-ring cider consult deft
+                   embark embark-consult exec-path-from-shell git-timemachine
+                   gptel haskell-mode magit marginalia markdown-mode
+                   material-theme mmm-mode orderless paredit projectile
+                   rainbow-delimiters scss-mode smart-mode-line solarized-theme
+                   swiper typescript-mode undo-tree vertico visual-fill-column
+                   visual-regexp web-mode yaml-mode yasnippet)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(markdown-header-face ((t (:inherit font-lock-function-name-face :weight bold :family "variable-pitch"))))
+ '(markdown-header-face-1 ((t (:inherit markdown-header-face :height 1.7))))
+ '(markdown-header-face-2 ((t (:inherit markdown-header-face :height 1.4))))
+ '(markdown-header-face-3 ((t (:inherit markdown-header-face :height 1.2)))))
 (put 'narrow-to-region 'disabled nil)
